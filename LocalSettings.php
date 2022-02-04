2306011269e50deabc4aa381b2acfe4f02d043e7
Alex Hockly <alhockly@gmail.com>
2022-02-04 19:02:25 +0000

    added config

---
+++ LocalSettings.php
@@ -0,0 +1,162 @@
+# Protect against web entry
+if ( !defined( 'MEDIAWIKI' ) ) {
+exit;
+}
+
+
+## Uncomment this to disable output compression
+# $wgDisableOutputCompression = true;
+
+$wgSitename = "CannaWiki";
+
+## The URL base path to the directory containing the wiki;
+## defaults for all runtime URL paths are based off of this.
+## For more information on customizing the URLs
+## (like /w/index.php/Page_title to /wiki/Page_title) please see:
+## https://www.mediawiki.org/wiki/Manual:Short_URL
+$wgScriptPath = "/CannaWiki";
+$wgFavicon = "/CannaWiki/favicon.ico";
+
+## The protocol and server name to use in fully-qualified URLs
+$wgServer = "https://cannawiki.net";
+
+## The URL path to static resources (images, scripts, etc.)
+$wgResourceBasePath = $wgScriptPath;
+
+## The URL paths to the logo.  Make sure you change this from the default,
+## or else you'll overwrite your logo when you upgrade!
+$wgLogos = [ '1x' => "$wgResourceBasePath/resources/assets/wiki.png" ];
+
+## UPO means: this is also a user preference option
+
+$wgEnableEmail = true;
+$wgEnableUserEmail = true; # UPO
+
+$wgEmergencyContact = "apache@.invalid";
+$wgPasswordSender = "apache@.invalid";
+
+$wgEnotifUserTalk = false; # UPO
+$wgEnotifWatchlist = false; # UPO
+$wgEmailAuthentication = true;
+
+## Database settings
+$wgDBtype = "mysql";
+$wgDBserver = env("CLEARDB_DATABASE_URL");
+$wgDBname = "cannawiki";
+$wgDBuser = "root";
+$wgDBpassword = "";
+
+# MySQL specific settings
+$wgDBprefix = "";
+
+# MySQL table options to use during installation or update
+$wgDBTableOptions = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
+
+# Shared database table
+# This has no effect unless $wgSharedDB is also set.
+$wgSharedTables[] = "actor";
+
+## Shared memory settings
+$wgMainCacheType = CACHE_NONE;
+$wgMemCachedServers = [];
+
+## To enable image uploads, make sure the 'images' directory
+## is writable, then set this to true:
+$wgEnableUploads = true;
+#$wgUseImageMagick = true;
+#$wgImageMagickConvertCommand = "/usr/bin/convert";
+
+# InstantCommons allows wiki to use images from https://commons.wikimedia.org
+$wgUseInstantCommons = false;
+
+# Periodically send a pingback to https://www.mediawiki.org/ with basic data
+# about this MediaWiki instance. The Wikimedia Foundation shares this data
+# with MediaWiki developers to help guide future development efforts.
+$wgPingback = true;
+
+## If you use ImageMagick (or any other shell command) on a
+## Linux server, this will need to be set to the name of an
+## available UTF-8 locale. This should ideally be set to an English
+## language locale so that the behaviour of C library functions will
+## be consistent with typical installations. Use $wgLanguageCode to
+## localise the wiki.
+$wgShellLocale = "C.UTF-8";
+
+## Set $wgCacheDirectory to a writable directory on the web server
+## to make your wiki go slightly faster. The directory should not
+## be publicly accessible from the web.
+#$wgCacheDirectory = "$IP/cache";
+
+# Site language code, should be one of the list in ./languages/data/Names.php
+$wgLanguageCode = "en";
+
+$wgSecretKey = env("SECRET_KEY");
+
+# Changing this will log out all existing sessions.
+$wgAuthenticationTokenVersion = "1";
+
+# Site upgrade key. Must be set to a string (default provided) to turn on the
+# web installer while LocalSettings.php is in place
+$wgUpgradeKey = "46d61dc2c31eee3e";
+
+## For attaching licensing metadata to pages, and displaying an
+## appropriate copyright notice / icon. GNU Free Documentation
+## License and Creative Commons licenses are supported so far.
+$wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
+$wgRightsUrl = "";
+$wgRightsText = "";
+$wgRightsIcon = "";
+
+# Path to the GNU diff3 utility. Used for conflict resolution.
+$wgDiff3 = "";
+
+## Default skin: you can change the default skin. Use the internal symbolic
+## names, ie 'vector', 'monobook':
+$wgDefaultSkin = "vector";
+
+# Enabled skins.
+# The following skins were automatically enabled:
+wfLoadSkin( 'MonoBook' );
+wfLoadSkin( 'Timeless' );
+wfLoadSkin( 'Vector' );
+wfLoadSkin( 'MinervaNeue' );
+wfLoadSkin( 'Tweeki' );
+
+
+# End of automatically generated settings.
+# Add more configuration options below.
+wfLoadExtension( 'Moderation' );
+wfLoadExtension( 'Cite' );
+wfLoadExtension( 'MobileFrontend' );
+$wgMFDefaultSkinClass = 'SkinMinerva';
+
+
+$wgGroupPermissions['*']['edit'] = false;
+
+
+require_once "$IP/extensions/HeadScript/HeadScript.php";
+
+
+$wgHeadScriptCode = <<<'START_END_MARKER'
+<script async src="https://www.googletagmanager.com/gtag/js?id="+env("GOOGLE_ANALYTICS_KEY")></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', env("GOOGLE_ANALYTICS_KEY"));
+</script>
+START_END_MARKER;
+
+
+// Discord bot
+wfLoadExtension( 'Discord' );
+$wgDiscordWebhookURL = ['https://discordapp.com/api/webhooks/937367961537220608/BVAroukUFSVv6M4D69cLde6nkkTpX3L07frhvs9nnDLAPHD-FdPPSZiNOQL8wtL61HOC'];
+
+
+wfLoadExtension( 'WebToolsManager' );
+
+wfLoadExtension( 'GoogleRichCards' );
+
+wfLoadExtension( 'VisualEditor' );
+
+wfLoadExtension( 'SimpleBatchUpload' );
